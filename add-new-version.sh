ec3a7be49717f8c8cdcdc5fb9b1362bae057de85
Martin Grigorov <mgrigorov@openeuler.sh>
2024-01-25 14:27:56 +0200

    Add Docker files for 22.03-lts-sp3 aarch64
    Signed-off-by: Martin Grigorov <mgrigorov@openeuler.sh>

--- add-new-version.sh
+++ add-new-version.sh
@@ -7,17 +7,25 @@
 
 set -e
 
+CYAN="\033[0;36m"
+RED="\033[0;31m"
+NC='\033[0m' # No Color
+
 die() {
-    echo "Error: $1";
+    echo -e "${RED}Error: $1${NC}";
     exit 1;
+}
+
+info() {
+    echo -e "${CYAN}$1${NC}"
 }
 
 archs="x86_64 aarch64"
 VERSION=$1
 
 if [[ -z ${VERSION} ]]; then
-  echo "Usage: $0 <version>";
-  echo -e "\nExample: $0 24.03-lts";
+  info "Usage: $0 <version>";
+  info "\nExample: $0 24.03-lts";
   exit 1
 fi
 
@@ -28,8 +36,7 @@ do
     elif [[ "$ARCH" = "x86_64" ]];then
         DOCKER_ARCH=amd64
     else
-        echo "Unknown arch: ${ARCH}"
-        exit 1
+        die "Unknown arch: ${ARCH}"
     fi
     
     mkdir -p ${VERSION}
@@ -38,30 +45,41 @@ do
     pushd ${VERSION}
     URL_VERSION=`echo ${VERSION} | tr 'a-z' 'A-Z'`
     if [ ! -f "openEuler-docker.${ARCH}.tar.xz" ]; then
+        info "Going to download openEuler-docker.${ARCH}.tar.xz"
         wget --quiet https://repo.openeuler.org/openEuler-${URL_VERSION}/docker_img/${ARCH}/openEuler-docker.${ARCH}.tar.xz || die "Cannot find version ${VERSION}!";
     fi
 
     # Re-download and validate sha256sum everytime
     rm -f openEuler-docker.$ARCH.tar.xz.sha256sum
+    info "Going to download openEuler-docker.$ARCH.tar.xz.sha256sum"
     wget --quiet https://repo.openeuler.org/openEuler-${URL_VERSION}/docker_img/${ARCH}/openEuler-docker.${ARCH}.tar.xz.sha256sum || die "Cannot download sha256sum for ${VERSION}!";
     shasum -c openEuler-docker.${ARCH}.tar.xz.sha256sum
     
-    # Extract rootfs
     if [ ! -f "openEuler-docker-rootfs.${DOCKER_ARCH}.tar.xz" ]; then
+        info "Going to extract the rootfs.tar.xz ..."
         tar -xf openEuler-docker.${ARCH}.tar.xz --wildcards "*.tar" --exclude "layer.tar"
         ROOT_FS=`ls | xargs -n1 | grep -v openEuler |grep *.tar`
         mv ${ROOT_FS} openEuler-docker-rootfs.${DOCKER_ARCH}.tar
         xz -z openEuler-docker-rootfs.${DOCKER_ARCH}.tar
     fi
-    NEW_DOCKERFILE="Dockerfile-${VERSION}-${ARCH}"
-    cp -f ../Dockerfile-22.03-lts-${ARCH} ./${NEW_DOCKERFILE}
-    sed -i "s/22.03-lts/${VERSION}/" ${NEW_DOCKERFILE}
+    DOCKERFILE="Dockerfile-${VERSION}-${ARCH}"
+    cat <<EOF > ${DOCKERFILE} 
+FROM scratch
 
-    cp -f ${NEW_DOCKERFILE} ../
+ADD openEuler-${VERSION}-docker-rootfs.aarch64.tar.xz /
+RUN sed -i "s@repo.openeuler.org@repo.huaweicloud.com/openeuler@g" /etc/yum.repos.d/openEuler.repo
+# See more in https://gitee.com/openeuler/cloudnative/issues/I482Q6
+RUN ln -sf /usr/share/zoneinfo/UTC /etc/localtime && \
+    sed -i "s/TMOUT=300/TMOUT=0/g" /etc/bashrc && \
+    yum -y update && yum clean all
+CMD ["bash"]
+EOF
+
+    cp -f ${DOCKERFILE} ../
     cp -f openEuler-docker-rootfs.${DOCKER_ARCH}.tar.xz ../openEuler-${VERSION}-docker-rootfs.${ARCH}.tar.xz
-    git add ../${NEW_DOCKERFILE} ../openEuler-${VERSION}-docker-rootfs.${ARCH}.tar.xz
+    git add ../${DOCKERFILE} ../openEuler-${VERSION}-docker-rootfs.${ARCH}.tar.xz
 
     popd
 done
 
-echo -e "\nDone! Please review the new changes with 'git diff' and commit+push them if they look good!"
+info "\nDone! Please review the new changes with 'git diff' and commit+push them if they look good!"
